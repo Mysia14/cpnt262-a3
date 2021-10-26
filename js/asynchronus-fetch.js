bf1ad9084ba9b922eb445d8eca1aaef34bb53459
Mysia <mysiasantana@gmail.com>
2021-10-26 14:50:25 -0600

    added functions

--- js/asynchronus-fetch.js
+++ js/asynchronus-fetch.js
@@ -1,29 +1,67 @@
-fetch('https://animechan.vercel.app/api/random')
+//created a variable for the memes
 
-.then(function(response) {
-        // JSON that is returned from the server must be converted to a JS object asynchronously.
+const Meme1 = document.querySelector(".memebtn")
+
+// created a variable for the "error"
+
+const ErrorMeme = document.querySelector(".error")
+
+// Function to fetch data
+
+const PickMeme = function() {
+        fetch('https://api.imgflip.com/get_memes')
+
+        .then(function(response) {
+
+            // JSON that is returned from the server will be converted to a JS object asynchronously.
+            //console.log(response);
+            if (!response.ok) {
+                throw new Error('Not 200 OK');
+            }
+            return response.json();
+        })
 
 
-        console.log(response);
-        if (!response.ok) {
-            throw new Error('Not 200 OK');
+        // load the Memes images
+
+        .then(function(data) {
+                return fetch('https://api.imgflip.com/get_memes')
+            })
+            .then(function(response) {
+                // check if response is ok
+                if (!response.ok) {
+                    // if not throw an error
+                    throw new Error('Not 200 OK');
+                }
+                // return new json file
+                return response.json();
+            })
+            .then(function(data) {
+                // Any code that depends on the `data` must go in this block
+                const element = document.querySelector(".meme");
+            })
+            // output the variable 
+        let output = " ";
+
+        // loop 
+        for (let i = 0; i <= 1; i++) {
+            output += `<figure> <img src=" ${data.url}" alt="Meme">
+        <figcaption> Name:${data.name}.</figcaption></figure>`
         }
-        return response.json();
-    })
-    .then(function(data) {
-        // Any code that depends on the `data` must go in this block
 
-        const element = document.querySelector(".anime");
 
-        element.innerHTML = `
-        <figure>
-        <img src=" ${data.url}" alt="anime image">
-        <figcaption>${data.fileSize} bytes os data.</figcaption>figure>`;
-        console.log(data);
-    })
+        //output
+        element.innerHTML = `<fieldset><legend>MemGif</legend>${output}</fieldset>`;
+
+        // An error or `reject ` from any of the above `.then()` blocks will end up here.
+
+    }
     .catch(function(err) {
-        // An error or `reject` from any of the above `.then()` blocks will end up here.
-        console.log(err);
+        Meme1.textContent = "Failed!"
+        ErrorMeme.innerHTML = "Ops! Something went wrong"
+
     });
 
-//const button = document.querySelector("button").addEventListener ("click, fetch")
+// Calling the Memes images
+
+Meme1.addEventListener("click, PickMeme");
