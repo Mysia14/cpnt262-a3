d9c2f49619c8b5348690511f1cd62b7d2a2cfbe7
Mysia <mysiasantana@gmail.com>
2021-10-28 16:16:26 -0600

    added math.random function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cpnt262-a3
+
+Course title;
+Author name;
+Links to:
+your GH repo;
+Gh Pages link;
+Any comments that may help squeeze marks out of your instructor (maybe give him hints on where to look for the requirements above);
+Attributions for any code or assets that are not your own.
--- js/asynchronus-fetch.js
+++ js/asynchronus-fetch.js
@@ -1,67 +1,54 @@
 //created a variable for the memes
 
-const Meme1 = document.querySelector(".memebtn")
+const Meme1 = document.querySelector(".memebtn");
 
 // created a variable for the "error"
 
-const ErrorMeme = document.querySelector(".error")
+const ErrorMeme = document.querySelector(".error");
 
 // Function to fetch data
 
 const PickMeme = function() {
-        fetch('https://api.imgflip.com/get_memes')
+    fetch('https://api.imgflip.com/get_memes')
 
-        .then(function(response) {
+    .then(function(response) {
 
-            // JSON that is returned from the server will be converted to a JS object asynchronously.
-            //console.log(response);
-            if (!response.ok) {
-                throw new Error('Not 200 OK');
-            }
-            return response.json();
-        })
-
-
-        // load the Memes images
-
-        .then(function(data) {
-                return fetch('https://api.imgflip.com/get_memes')
-            })
-            .then(function(response) {
-                // check if response is ok
-                if (!response.ok) {
-                    // if not throw an error
-                    throw new Error('Not 200 OK');
-                }
-                // return new json file
-                return response.json();
-            })
-            .then(function(data) {
-                // Any code that depends on the `data` must go in this block
-                const element = document.querySelector(".meme");
-            })
-            // output the variable 
-        let output = " ";
-
-        // loop 
-        for (let i = 0; i <= 1; i++) {
-            output += `<figure> <img src=" ${data.url}" alt="Meme">
-        <figcaption> Name:${data.name}.</figcaption></figure>`
+        // JSON that is returned from the server will be converted to a JS object asynchronously.
+        //console.log(response);
+        if (!response.ok) {
+            throw new Error('Not 200 OK');
         }
+        return response.json();
+    })
 
+    // load the Memes images
+
+    .then(function(data) {
+        console.log(data.data.memes[1])
+            // Created a variable for the a random element
+        const element = document.querySelector(".meme");
+        let output = " ";
+        const randomNumber = Math.floor(Math.random() * 100);
+
+        // loop
+
+        output = `<figure> <img src=" ${data.data.memes[1].url}" alt="${data.data.memes[1].name}">
+        <figcaption> :${data.data.memes[1].name}.</figcaption></figure>`;
 
         //output
         element.innerHTML = `<fieldset><legend>MemGif</legend>${output}</fieldset>`;
 
+    })
+
+    randomNumber()
         // An error or `reject ` from any of the above `.then()` blocks will end up here.
+        .catch(function(err) {
+            Meme1.textContent = "Failed!"
+            ErrorMeme.innerHTML = "Ops! Something went wrong"
 
-    }
-    .catch(function(err) {
-        Meme1.textContent = "Failed!"
-        ErrorMeme.innerHTML = "Ops! Something went wrong"
+        });
 
-    });
+    // Calling the Memes images
+}
 
-// Calling the Memes images
-
-Meme1.addEventListener("click, PickMeme");
+Meme1.addEventListener("click", PickMeme);
